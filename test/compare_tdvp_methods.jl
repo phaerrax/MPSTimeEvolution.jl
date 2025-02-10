319b23621e1f10ce2d32a327dde503e5ea23c683
Davide Ferracin <davide.ferracin@protonmail.com>
2025-02-10 17:37:06 +0100

    test: use ITensor's TDVP as reference
    Simulate the small test SIAM also with ITensor's own TDVP function and
    use that as a reference for the functions from this package.

--- test/compare_tdvp_methods.jl
+++ test/compare_tdvp_methods.jl
@@ -1,16 +1,88 @@
 using CSV
+using Observers: observer
+
+jws(n1, n2) = (i => "F" for i in (n1 + 1):(n2 - 1))  # Jordan-Wigner string
+
+function itensors_tdvp(; dt, tmax, N, check_sites, init)
+    sites = siteinds("Fermion", N)
+
+    state_0 = MPS(sites, init)
+    maxbonddim = maximum(maxlinkdims(state_0))
+    state_0 = enlargelinks(state_0, maxbonddim; ref_state=init)
+    h = spin_chain([0.5; fill(0.25, N)], fill(1, N), sites)
+    H = MPO(h, sites)
+
+    site_pairs = [
+        (check_sites[j], check_sites[i]) for i in eachindex(check_sites) for j in 1:(i - 1)
+    ]
+    cc_ops = [
+        [
+            begin
+                os = OpSum()
+                # In the other test functions below we measure, with our TDVP,
+                #
+                #   a(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ adag(n2),
+                #   adag(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ a(n2)
+                #
+                # for all `n1 < n2` from `check_sites`. With the `c` operators we'd get
+                #
+                #   cdag(n1) c(n2) = adag(n1)f(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ adag(n2)
+                #                  = adag(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ adag(n2),
+                #   c(n1) cdag(n2) = a(n1)f(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ adag(n2)
+                #                  = -a(n1) ⊗ f(n1+1) ⊗ ⋯ ⊗ f(n2-1) ⊗ adag(n2)
+                #
+                # so in order to have the same results we need an additional minus sign.
+                os += (-1, "c", n1, "cdag", n2)
+                MPO(os, sites)
+            end for (n1, n2) in site_pairs
+        ]
+        [
+            begin
+                os = OpSum()
+                os += ("cdag", n1, "c", n2)
+                MPO(os, sites)
+            end for (n1, n2) in site_pairs
+        ]
+    ]
+    current_time(; current_time) = real(im * current_time)
+    measure_n(; state) = expect(state, "n"; sites=check_sites)
+    measure_cc(; state) = [inner(state', obs, state) for obs in cc_ops]
+
+    obs = observer("t" => current_time, "n" => measure_n, "cc" => measure_cc)
+    ITensorMPS.tdvp(
+        H,
+        -im * tmax,
+        state_0;
+        time_step=-im * dt,
+        (step_observer!)=obs,
+        outputlevel=0,
+        updater_kwargs=(; tol=1e-14, krylovdim=30, maxiter=100),  # same as our defaults
+    )
+
+    # We also need to add the initial expectation values---ITensors' TDVP doesn't measure
+    # them. (Maybe there's a way we can do this via the observer...)
+    results = [
+        [
+            [expect(state_0, "n"; sites=check_sites[n]); [row[n] for row in obs.n]] for
+            n in eachindex(check_sites)
+        ]
+        [
+            [inner(state_0', cc_ops[n], state_0); [row[n] for row in obs.cc]] for
+            n in eachindex(cc_ops)
+        ]
+    ]
+
+    return [0.0; obs.t], results...
+end
 
 # We define a small SIAM system and evolve it with our different TDVP methods.
 # We choose a single fermion interacting with just a single environment: we need to focus
 # on the correctness of the TDVP functions, mostly from a syntactic point of view. It
 # doesn't have to be a physically interesting, or correct, system.
 
-jws(n1, n2) = (i => "F" for i in (n1 + 1):(n2 - 1))  # Jordan-Wigner string
+function siam_tdvp1(; dt, tmax, N, check_sites, init)
+    sites = siteinds("Fermion", N)
 
-function siam_tdvp1(; dt, tmax, N, check_sites)
-    sites = siteinds("Fermion", N + 1)
-
-    init(n) = n == 1 ? "Occ" : "Emp"
     state_0 = MPS(sites, init)
     maxbonddim = maximum(maxlinkdims(state_0))
     state_0 = enlargelinks(state_0, maxbonddim; ref_state=init)
@@ -56,10 +128,9 @@ function siam_tdvp1(; dt, tmax, N, check_sites)
     return f["time"], results...
 end
 
-function siam_tdvp1_with_qns(; dt, tmax, N, check_sites)
-    sites = siteinds("Fermion", N + 1; conserve_nfparity=true)
+function siam_tdvp1_with_qns(; dt, tmax, N, check_sites, init)
+    sites = siteinds("Fermion", N; conserve_nfparity=true)
 
-    init(n) = n == 1 ? "Occ" : "Emp"
     state_0 = MPS(sites, init)
     maxbonddim = maximum(maxlinkdims(state_0))
     state_0 = enlargelinks(state_0, maxbonddim; ref_state=init)
@@ -105,12 +176,12 @@ function siam_tdvp1_with_qns(; dt, tmax, N, check_sites)
     return f["time"], results...
 end
 
-function siam_tdvp1vec(; dt, tmax, N, check_sites)
-    sites = siteinds("vFermion", N + 1)
+function siam_tdvp1vec(; dt, tmax, N, check_sites, init)
+    sites = siteinds("vFermion", N)
 
-    state_0 = MPS(sites, n -> n == 1 ? "Occ" : "Emp")
+    state_0 = MPS(sites, init)
     maxbonddim = maximum(maxlinkdims(state_0))
-    state_0 = enlargelinks(state_0, maxbonddim; ref_state=n -> n == 1 ? "Occ" : "Emp")
+    state_0 = enlargelinks(state_0, maxbonddim; ref_state=init)
 
     ℓ = spin_chain([0.5; fill(0.25, N)], fill(1, N), sites)
     L = MPO(ℓ, sites)
@@ -153,16 +224,19 @@ function siam_tdvp1vec(; dt, tmax, N, check_sites)
     return f["time"], results...
 end
 
-function siam_tdvp1vec_superfermions(; dt, tmax, N, check_sites)
+function siam_tdvp1vec_superfermions(; dt, tmax, N, check_sites, init)
     sf_index(n) = 2n - 1
+    inv_sf_index(n) = div(n + 1, 2)
     # (2n-1)-th site with superfermions == n-th site with traditional fermions
+    # `inv_sf_index` is the left-inverse of `sf_index`:
+    #   sf_index.(inv_sf_index.(1:10)) == [1, 1, 3, 3, 5, 5, 7, 7, 9, 9]
+    #   inv_sf_index.(sf_index.(1:10)) == [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-    sites = siteinds("Fermion", 2(N + 1); conserve_nfparity=true)
+    sites = siteinds("Fermion", 2N; conserve_nfparity=true)
 
-    init(n) = div(n + 1, 2) == 1 ? "Occ" : "Emp"
-    state_0 = MPS(sites, init)
+    state_0 = MPS(sites, init ∘ inv_sf_index)
     maxbonddim = maximum(maxlinkdims(state_0))
-    state_0 = enlargelinks(state_0, maxbonddim; ref_state=init)
+    state_0 = enlargelinks(state_0, maxbonddim; ref_state=init ∘ inv_sf_index)
 
     ℓ =
         spin_chain([0.5; fill(0.25, N)], fill(1, N), sites[1:2:end]) -
@@ -212,10 +286,10 @@ function siam_tdvp1vec_superfermions(; dt, tmax, N, check_sites)
     return f["time"], results...
 end
 
-function siam_adjtdvp1vec(; dt, tmax, N, check_sites)
-    sites = siteinds("vFermion", N + 1)
+function siam_adjtdvp1vec(; dt, tmax, N, check_sites, init)
+    sites = siteinds("vFermion", N)
 
-    state_0 = MPS(sites, n -> n == 1 ? "Occ" : "Emp")
+    state_0 = MPS(sites, init)
 
     adjℓ = spin_chain′([0.5; fill(0.25, N)], fill(1, N), sites)
     adjL = MPO(adjℓ, sites)
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using MPSTimeEvolution
 using Test
-using ITensors, ITensorMPS, LindbladVectorizedTensors
+using ITensors, ITensorMPS, LindbladVectorizedTensors, Observers
 
 include("compare_tdvp_methods.jl")
 # This tests pushes the bond dimension to the maximum admitted by the sizes of the system,
@@ -9,15 +9,29 @@ include("compare_tdvp_methods.jl")
     dt = 0.01
     tmax = 0.5
     N = 4
-    s = [1, 3]
-    res_tdvp1 = siam_tdvp1(; dt=dt, tmax=tmax, N=N, check_sites=s)  # reference
+    alternate(n) = isodd(n) ? "Occ" : "Emp"
+    sites = [1, 3]
+    # It's best to choose sites that start from an occupied state, to avoid slight numerical
+    # instabilities that could make `isapprox` fail.
+    reference_result = itensors_tdvp(;
+        dt=dt, tmax=tmax, N=N, check_sites=sites, init=alternate
+    )
 
-    res_tdvp1vec_sf = siam_tdvp1vec_superfermions(; dt=dt, tmax=tmax, N=N, check_sites=s)
-    @test all(res_tdvp1 .≈ res_tdvp1vec_sf)
-    res_tdvp1vec = siam_tdvp1vec(; dt=dt, tmax=tmax, N=N, check_sites=s)
-    @test all(res_tdvp1 .≈ res_tdvp1vec)
-    res_adjtdvp1vec = siam_adjtdvp1vec(; dt=dt, tmax=tmax, N=N, check_sites=s)
-    @test all(res_tdvp1 .≈ res_adjtdvp1vec)
+    res_tdvp1 = siam_tdvp1(; dt=dt, tmax=tmax, N=N, check_sites=sites, init=alternate)
+    @test all(reference_result .≈ res_tdvp1)
+
+    res_tdvp1vec_sf = siam_tdvp1vec_superfermions(;
+        dt=dt, tmax=tmax, N=N, check_sites=sites, init=alternate
+    )
+    @test all(reference_result .≈ res_tdvp1vec_sf)
+
+    res_tdvp1vec = siam_tdvp1vec(; dt=dt, tmax=tmax, N=N, check_sites=sites, init=alternate)
+    @test all(reference_result .≈ res_tdvp1vec)
+
+    res_adjtdvp1vec = siam_adjtdvp1vec(;
+        dt=dt, tmax=tmax, N=N, check_sites=sites, init=alternate
+    )
+    @test all(reference_result .≈ res_adjtdvp1vec)
 end
 
 include("joint_tdvp.jl")
