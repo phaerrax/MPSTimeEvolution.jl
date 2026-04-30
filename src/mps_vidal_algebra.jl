ca579fef667fb12611e9dd92764c4240db94a62f
Davide Ferracin <davide.ferracin@protonmail.com>
2026-04-30 20:41:51 +0200

    feat: truncate Vidal MPSs
    Add a `truncate` method that uses the SVD to compress an MPS in the
    Vidal form, modelled after the homonymous function from ITensor for
    standard MPSs.

--- src/mps_vidal_algebra.jl
+++ src/mps_vidal_algebra.jl
@@ -1,3 +1,5 @@
+### Inner products
+
 function LinearAlgebra.dot(ψ1::VidalMPS, ψ2::VidalMPS)::Number
     N = nsites(ψ1)
     if nsites(ψ2) != N
@@ -49,4 +51,58 @@ function LinearAlgebra.norm(ψ::VidalMPS)
             "may not be accurate."
     end
     return sqrt(real(norm2_ψ))
+end
+
+### Truncation
+
+"""
+    truncate!(M::VidalMPS; kwargs...)
+
+Perform a truncation of all bonds of a VidalMPS using the truncation parameters
+(`cutoff`, `maxdim`, etc.) provided as keyword arguments.
+
+Keyword arguments:
+
+- `site_range=1:nsites(ψ)` - only truncate the bonds between these sites
+- `callback=Returns(nothing)` - callback function that allows the user to save the
+  per-bond truncation error. The API of `callback` expects to take two kwargs called `link`
+  and `truncation_error` where `link` is of type `Pair{Int64, Int64}` and `truncation_error`
+  is `Float64`. Consider the following example that illustrates one possible use case.
+
+```julia
+nbonds = 9
+truncation_errors = zeros(nbonds)
+function callback(; link, truncation_error)
+    bond_no = last(link)
+    truncation_errors[bond_no] = truncation_error
+    return nothing
+end
+truncate!(ψ; maxdim = 5, cutoff = 1E-7, callback)
+```
+"""
+function ITensorMPS.truncate!(
+    ψ::VidalMPS; site_range=1:nsites(ψ), callback=Returns(nothing), kwargs...
+)
+    site_ts = site_tensors(ψ)
+    bond_ts = bond_tensors(ψ)
+    # Perform truncations from left to right.
+    for j in first(site_range):(last(site_range) - 1)
+        linds = uniqueinds(site_ts[j], bond_ts[j] * site_ts[j + 1])
+        ltags = tags(commonind(site_ts[j], bond_ts[j] * site_ts[j + 1]))
+        rtags = tags(commonind(site_ts[j] * bond_ts[j], site_ts[j + 1]))
+        U, S, V, spec = svd(
+            site_ts[j] * bond_ts[j], linds; lefttags=ltags, righttags=rtags, kwargs...
+        )
+        site_ts[j] = U
+        bond_ts[j] = S
+        site_ts[j + 1] *= V
+        callback(; link=(j => j - 1), truncation_error=spec.truncerr)
+    end
+    return ψ
+end
+
+function ITensorMPS.truncate(ψ0::VidalMPS; kwargs...)
+    ψ = copy(ψ0)
+    truncate!(ψ; kwargs...)
+    return ψ
 end
