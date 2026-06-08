37c9f1b0c6a8d2f389df9f56fc6202b6ceeccb13
Davide Ferracin <davide.ferracin@protonmail.com>
2026-06-08 20:27:25 +0200

    refactor: use MPI's own `bcast` and `gather`
    Use `MPI.bcast` and `MPI.gather` instead of the functions taken from
    ITensorParallel.jl, since both perform the same operation and can handle
    arbitrary data.

--- ext/MPSTimeEvolutionMPIExt/parallel_tdvp2/main.jl
+++ ext/MPSTimeEvolutionMPIExt/parallel_tdvp2/main.jl
@@ -48,10 +48,12 @@ function MPSTimeEvolution.tdvp2_parallel!(
         end
 
         io_handle = MPSTimeEvolution.writeheaders_data(io_file, cb; kwargs...)
-        ranks_handle = MPSTimeEvolution.writeheaders_ranks(ranks_file, length(ψ))
+        isnothing(io_file) || @debug "Initialising output file $io_file..."
+        ranks_handle = MPSTimeEvolution.writeheaders_ranks(ranks_file, nsites(ψ))
         times_handle = MPSTimeEvolution.writeheaders_stime(times_file)
 
         # Measure everything once in the initial state.
+        @debug "Computing expectation values on initial state..."
         MPSTimeEvolution.apply!(cb, ψ, TDVP2(); current_time=current_time)
         MPSTimeEvolution.compute_norm!(cb, ψ, TDVP2(); current_time=current_time)
 
@@ -117,8 +119,8 @@ function MPSTimeEvolution.tdvp2_parallel!(
             MPI.Barrier(comm)  # Wait for everyone to finish...
 
             @debug "Step $s complete. Gathering data from workers..."
-            sts = gather(parent(st), rootrank, comm)
-            bts = gather(parent(bt), rootrank, comm)
+            sts = MPI.gather(parent(st), comm; root=rootrank)
+            bts = MPI.gather(parent(bt), comm; root=rootrank)
 
             # Recompose the MPS so that we can compute expectation values later.
             # return
@@ -132,7 +134,7 @@ function MPSTimeEvolution.tdvp2_parallel!(
             end
         end
         isroot && @debug "Broadcasting the MPS to all workers..."
-        ψ = bcast(ψ, rootrank, comm)
+        ψ = MPI.bcast(ψ, comm; root=rootrank)
         # Broadcast the updates MPS to all workers, so that they can use it when the new
         # step begins.
 
